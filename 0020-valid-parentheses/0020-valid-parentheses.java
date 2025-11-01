b324e41cb81ff416d5e1c7b6b9bf087940b1741d
DEEPAN  B <deepanb2005@gmail.com>
2025-11-01 11:18:17 +0530

    Time: 3 ms (59.44%), Space: 43.2 MB (5.3%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character>sk=new Stack<>();
+        for(int i=0;i<s.length();i++)
+        {
+            char a=s.charAt(i);
+            if(a=='['||a=='('||a=='{')
+            {
+                sk.push(a);
+            }
+            else
+            {
+                if(sk.isEmpty())
+                {
+                    sk.push(a);
+                }
+                else
+                {
+                    char t=sk.peek();
+                    if( t=='[' && a==']' || t=='{' && a=='}' || t=='(' && a==')')
+                    {
+                        sk.pop();
+                    }
+                    else{return false;}
+                }
+            }
+        }
+        return sk.isEmpty();
+    }
+}
