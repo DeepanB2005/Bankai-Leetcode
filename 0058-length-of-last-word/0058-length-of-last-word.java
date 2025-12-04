d960e89d9dc13dcfdbe5483f74f8201cd1d430f3
DEEPAN  B <deepanb2005@gmail.com>
2025-12-04 21:00:10 +0530

    Time: 0 ms (100%), Space: 42.9 MB (30.92%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int c=0;
+        int i=s.length()-1;
+        while(s.charAt(i)==' ')
+        {
+            i--;
+        }
+        while(i>=0 && s.charAt(i)!=' ')
+        {
+            c+=1;
+            i-=1;
+
+        }
+        return c;
+    }
+}
