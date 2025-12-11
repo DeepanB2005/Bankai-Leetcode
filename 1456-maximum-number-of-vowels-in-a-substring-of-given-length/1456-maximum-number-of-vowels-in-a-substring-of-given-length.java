30486e86e916821129bf0781e74b8810f99c1809
DEEPAN  B <deepanb2005@gmail.com>
2025-12-11 19:50:11 +0530

    Time: 13 ms (74.1%), Space: 46.4 MB (33.51%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,34 +1,22 @@
 class Solution {
+    boolean v(char a)
+    {
+        return a=='a'||a=='e'||a=='i'||a=='o'||a=='u';
+    }
+    int mv=0;
+    int c=0;
     public int maxVowels(String s, int k) {
-        HashSet<Character>set=new HashSet<>();
-        set.add('o');
-        set.add('i');
-        set.add('a');
-        set.add('e');
-        set.add('u');
-        int m=0;
-        int c=0;
-
         for(int i=0;i<k;i++)
         {
-            if(set.contains(s.charAt(i)))
-            {
-                c++;
-            }
+            if(v(s.charAt(i))) c++;
         }
-        m=c;
+        mv=c;
         for(int i=k;i<s.length();i++)
         {
-            if(set.contains(s.charAt(i-k)))
-            {
-                c--;
-            }
-            if(set.contains(s.charAt(i)))
-            {
-                c++;
-            }
-            m=m>c?m:c;
+            if(v(s.charAt(i-k)))c--;
+            if(v(s.charAt(i)))c++;
+            mv=mv>c?mv:c;
         }
-        return m;
+        return mv;
     }
 }
