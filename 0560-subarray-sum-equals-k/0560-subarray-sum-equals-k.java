ba4854352fd24d7315caf3612dca581a4480c0d2
DEEPAN  B <deepanb2005@gmail.com>
2026-01-04 14:02:23 +0530

    Time: 25 ms (46.9%), Space: 48.8 MB (47.49%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,8 +1,8 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        Map<Integer,Integer>map=new HashMap<>();
-        int s=0;
+        HashMap<Integer,Integer>map=new HashMap<>();
         int c=0;
+        int s=0;
         map.put(0,1);
         for(int i=0;i<nums.length;i++)
         {
@@ -14,5 +14,6 @@ class Solution {
             map.put(s,map.getOrDefault(s,0)+1);
         }
         return c;
+
     }
 }
