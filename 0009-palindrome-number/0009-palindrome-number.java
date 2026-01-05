900024ff4f74cf98dd3a36b9a89b657c7a035b74
DEEPAN  B <deepanb2005@gmail.com>
2026-01-06 00:27:01 +0530

    Time: 4 ms (99.86%), Space: 46.2 MB (17.43%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,14 +1,12 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x<0||x%10==0 && x!=0)return false;
-        int t=0;
+        int r=0;
         int o=x;
         while(x>0)
         {
-            int i=x%10;
-            t=t*10+i;
-            x=x/10;
+            r=r*10+x%10;
+            x/=10;
         }
-        return t==o;
+        return r==o;
     }
 }
