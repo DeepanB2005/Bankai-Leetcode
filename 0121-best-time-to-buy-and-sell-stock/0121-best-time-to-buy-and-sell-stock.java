48d2749610a97297ca29160bba34b12492adf629
DEEPAN  B <deepanb2005@gmail.com>
2026-01-07 20:12:58 +0530

    Time: 1 ms (99.75%), Space: 94.7 MB (14.99%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,17 +1,11 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int min =prices[0];
+        int min=prices[0];
         int max=0;
         for(int i=0;i<prices.length;i++)
         {
-            if(prices[i]<min)
-            {
-                min=prices[i];
-            }
-            else
-            {
-                max=prices[i]-min>max?prices[i]-min:max;
-            }
+            min=min<prices[i]?min:prices[i];
+            max=max>prices[i]-min?max:prices[i]-min;
         }
         return max;
     }
