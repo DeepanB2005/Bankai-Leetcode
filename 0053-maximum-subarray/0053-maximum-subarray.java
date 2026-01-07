a902308eff8bc98fca0d7481ee4cfdbeecf4e65d
DEEPAN  B <deepanb2005@gmail.com>
2026-01-07 20:45:28 +0530

    Time: 1 ms (99.72%), Space: 77.2 MB (49%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,12 +1,16 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int min=0;
-        int max=nums[0];
+        int m=nums[0];
+        int s=0;
         for(int i=0;i<nums.length;i++)
         {
-            min=min+nums[i]<nums[i]?nums[i]:min+nums[i];
-            max=max>min?max:min;
+            s+=nums[i];
+            m=m<s?s:m;
+            if(s<=0)
+            {
+                s=0;
+            }
         }
-        return max;
+        return m;
     }
 }
