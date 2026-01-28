8a6585187e561211980c2aa7e64c5b7415a1482e
DEEPAN  B <deepanb2005@gmail.com>
2026-01-28 19:49:28 +0530

    Time: 0 ms (100%), Space: 47.9 MB (44.79%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,24 +1,23 @@
 class Solution {
     public int trap(int[] height) {
+        int l=0;
+        int r=height.length-1;
         int lm=height[0];
         int rm=height[height.length-1];
         int w=0;
-        int l=0;
-        int r=height.length-1;
         while(l<r)
         {
             if(lm>rm)
             {
                 r--;
-                rm=Math.max(height[r],rm);
+                rm=rm>height[r]?rm:height[r];
                 w+=rm-height[r];
             }
             else
             {
                 l++;
-                lm=Math.max(height[l],lm);
+                lm=lm>height[l]?lm:height[l];
                 w+=lm-height[l];
-
             }
         }
         return w;
